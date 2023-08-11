7fb031913bf531ba81e22b662dae12c2a27b6e79
finacoa <107791878+finacoa@users.noreply.github.com>
2023-08-11 20:47:22 +0800

    Delete antivirus/mail directory

--- antivirus/mail/mcafee.py
+++
@@ -1,8 +0,0 @@
-from .mail_submission import MailSubmission
-
-
-class McAfee(MailSubmission):
-    name = "McAfee"
-    description = "Submit the file to McAfee for inclusion in detections."
-
-    mail_submission = "virus_research@mcafee.com"
--- antivirus/mail/sophos.py
+++
@@ -1,9 +0,0 @@
-from .mail_submission import MailSubmission
-
-
-class Sophos(MailSubmission):
-    name = "Sophos"
-    description = "Submit the file to Sophos for inclusion in detections."
-
-    mail_submission = "samples@sophos.com"
-    mail_subject = "Sample submitted for analysis - Reply needed"
